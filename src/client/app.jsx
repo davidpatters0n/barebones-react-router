6418381b1bc3111fdfa2e89fb75190f4931b8a5b
David Patterson <dpatterson2008@hotmail.com>
2016-12-12 22:41:06 +0000

    use es6 class definition

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,19 +1,23 @@
-
 import 'babel-polyfill';
-import React, { PropTypes } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import Person from '../shared/person';
 
 const newPerson = new Person('DJ').hello();
 
-const App = props => (
-  <div>
-    {props.message}
-  </div>
-);
+class Listing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: props.name };
+  }
 
-App.propTypes = {
-  message: PropTypes.string.isRequired,
+  render() {
+    return <div>{this.state.name}</div>;
+  }
+}
+
+Listing.propTypes = {
+  name: React.PropTypes.string.isRequired,
 };
 
-ReactDOM.render(<App message={newPerson} />, document.querySelector('.app'));
+ReactDOM.render(<Listing name={newPerson} />, document.querySelector('.app'));

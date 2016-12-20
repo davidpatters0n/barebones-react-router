85547d801f5f4299493510764d0721a468934a16
David Patterson <dpatterson2008@hotmail.com>
2016-12-20 01:25:31 +0000

    Add Delivery Details

--- src/client/delivery-details.jsx
+++ src/client/delivery-details.jsx
@@ -1,9 +1,66 @@
 import 'babel-polyfill';
-import React from 'react';
+import React, { PropTypes } from 'react';
 
-function DeliveryDetails() {
-  return <h1>Choose your delivery options here.</h1>;
+class DeliveryDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deliveryOption: 'Primary',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ deliveryOption: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.updateFormData(this.state);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Choose your delivery options here.</h1>
+        <div className="col-md-6">
+          <form onSubmit={this.handleSubmit}>
+            <div className="radio">
+              <label htmlFor="primaryOption">
+                <input
+                  type="radio"
+                  checked={this.state.deliveryOption === 'Primary'}
+                  value="Primary"
+                  onChange={this.handleChange}
+                />
+                Primary -- Next day delivery
+              </label>
+            </div>
+
+            <div className="radio">
+              <label htmlFor="normalOption">
+                <input
+                  type="radio"
+                  checked={this.state.deliveryOption === 'Normal'}
+                  value="Normal"
+                  onChange={this.handleChange}
+                />
+                Normal -- 3-5 Business Days
+              </label>
+            </div>
+
+            <button className="btn btn-success">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
 
+DeliveryDetails.propTypes = {
+  updateFormData: PropTypes.func,
+};
 
 export default DeliveryDetails;

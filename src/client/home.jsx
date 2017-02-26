51b2bc38d7e3255d58740f180ff0223f49529336
David Patterson <dpatterson2008@hotmail.com>
2017-02-26 14:52:11 -0500

    WIP

--- src/client/home.jsx
+++ src/client/home.jsx
@@ -1,33 +1,71 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars, no-debugger, no-console, no-undef */
 import React from 'react';
+import axios from 'axios';
 import Helmet from 'react-helmet';
-import { withGoogleMap, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import MarkerClusterer from 'react-google-maps/lib/addons/MarkerClusterer';
 
+// const SimpleMapExampleGoogleMap = withGoogleMap(props => (
+//   <GoogleMap
+//     defaultZoom={15}
+//     defaultCenter={{ lat: 51.418981, lng: -0.166303 }}
+//   />
+// ));
 
-const SimpleMapExampleGoogleMap = withGoogleMap(props => (
+const MarkerMap = withGoogleMap(props => (
   <GoogleMap
     defaultZoom={15}
     defaultCenter={{ lat: 51.418981, lng: -0.166303 }}
-  />
+  >
+    <MarkerClusterer
+      averageCenter
+      enableRetinaIcons
+      gridSize={60}
+    >
+      {props.markers.map(marker => (
+        <Marker
+          position={{ lat: marker.latitude, lng: marker.longitude }}
+          key={marker.photo_id}
+        />
+      ))}
+    </MarkerClusterer>
+  </GoogleMap>
 ));
 
-export default function Home() {
-  return (
-    <div className="row">
-      <div className="col-md-6" style={{ height: '500px' }}>
-        <Helmet title="Google Map Example" />
-        <div style={{ height: '100%' }}>
-          <SimpleMapExampleGoogleMap
-            containerElement={
-              <div style={{ height: '100%' }} />
-            }
-            mapElement={
-              <div style={{ height: '100%' }} />
-            }
-          />
-        </div>
-      </div>
-      <div className="col-md-6"><h1>TEST</h1></div>
-    </div>
-  );
+
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [],
+    };
+    // this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  componentDidMount() {
+    // axios.get('http://localhost:4567')
+    //   .then((res) => {
+    //     this.setState({ markers: res.data.responseData[0].foundATMLocations });
+    //   });
+    axios.get('https://gist.githubusercontent.com/farrrr/dfda7dd7fccfec5474d3/raw/758852bbc1979f6c4522ab4e92d1c92cba8fb0dc/data.json')
+      .then((res) => {
+        this.setState({ markers: res.photos });
+        // this.setState({ markers: res.data.responseData[0].foundATMLocations });
+      });
+  }
+
+  render() {
+    return (
+      <MarkerMap
+        containerElement={
+          <div style={{ height: '100%' }} />
+        }
+        mapElement={
+          <div style={{ height: '100%' }} />
+        }
+        markers={this.state.markers}
+      />
+    );
+  }
 }
